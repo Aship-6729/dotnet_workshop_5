b105d3b25f43c82a852cc156e253a98ba4857e8f
Aship-6729 <aalamaship98@gmail.com>
2025-11-26 18:35:20 +0545

    Task 4

---
+++ Task 4/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Bike bike = new Bike();
+            bike.Display();
+            bike.StartEngine();
+            bike.StopEngine();
+
+            Car car = new Car();
+            car.Display();
+            car.StartEngine();
+            car.StopEngine();
+
+        }
+    }
+}
