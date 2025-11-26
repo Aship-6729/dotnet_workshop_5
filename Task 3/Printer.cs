691cc13547409f3313e6a6b48fbcb523b579508f
Aship-6729 <aalamaship98@gmail.com>
2025-11-26 18:05:28 +0545

    Task 3

---
+++ Task 3/Printer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    internal class Printer
+    {
+        // Method Overloading since the method_name is same but frequency or the datatype of a parameter is differ.
+
+        // static method can be called directly by class-name.
+        public static void Print(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+
+        // non-static method needs an object to be called.
+        public void Print(int number)
+        {
+            Console.WriteLine(number);
+        }
+        public void Print(string message, int count)
+        {
+            Console.WriteLine($"{message} and {count}");
+        }   
+    }
+}
---
+++ Task 3/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            // since this print method is static, we can call it without creating an instance of the Printer class
+            Printer.Print("Welcome to Method Overloading in C#!");
+
+            Printer printer = new Printer();
+            printer.Print(42); // calling through the instance to non-static method to print an integer
+
+            printer.Print("This message will be printed", 3); // calling through the instance to non-static method to print a message and a count.
+        }
+    }
+}
