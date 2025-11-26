3b1b5544a1da3dbdef4fafd7dc6f09af1c243d2b
Aship-6729 <aalamaship98@gmail.com>
2025-11-26 11:57:50 +0545

    Task 1

---
+++ Task 1/BankAccount.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class BankAccount
+    {
+        private int accountNumber;
+        private double balance;
+
+        public BankAccount(int accountNumber, double balance)
+        {
+            this.accountNumber = accountNumber;
+            this.balance = balance;
+        }
+
+
+        public int GetAccountNumber()
+        {
+            return this.accountNumber;
+        }
+
+
+        public double GetBalance()
+        {
+            return this.balance;
+        }
+
+        private double SetBalance(int amount)
+        {
+            if (amount > 0)
+            {
+                this.balance = amount;
+                return this.balance;
+            }
+
+            return 1;
+
+        }
+
+
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                this.balance += amount;
+      
+            }
+            
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount > 0 && this.balance >= amount)
+            {
+                this.balance -= amount;
+            }
+            
+        }
+
+    }
+}
---
+++ Task 1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            BankAccount account = new BankAccount(1001,5000);
+            Console.WriteLine($"Account Number: {account.GetAccountNumber()}");
+
+            account.Deposit(500);
+            Console.WriteLine($"Balance after deposit: {account.GetBalance()}");
+
+            account.Withdraw(200);
+            Console.WriteLine($"Balance after withdrawal: {account.GetBalance()}");
+
+        }
+    }
+}
